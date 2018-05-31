30b5ec07bbe891bf78055c0421a4ff6870b11c70
최인훈 <torihome1@naver.com>
2018-05-31 19:16:03 +0900

    inhun hi

---
+++ insertion_sort.cpp
@@ -0,0 +1,17 @@
+#include <iosteam>
+
+using namespace std;
+
+void insertion_sort(int arr[], int length) {
+	int j, temp;
+
+	for (int i = 0; i < length; i++) {
+		j = i;
+
+		while (j > 0 && arr[j] < arr[j - 1]) {
+			temp = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = temp;
+			j--;
+		}
+	}
